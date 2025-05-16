a317d40a0049dd1e2728d718dc7d2a338a442c82
beatoz-kyle <kyle@beatoz.io>
2025-05-16 17:47:15 +0900

    feat: Fix deadlock in updating state

--- ctrlers/gov/ctrler_punish.go
+++ ctrlers/gov/ctrler_punish.go
@@ -1,6 +1,7 @@
 package gov
 
 import (
+	"fmt"
 	"github.com/beatoz/beatoz-go/ctrlers/gov/proposal"
 	v1 "github.com/beatoz/beatoz-go/ledger/v1"
 	"github.com/beatoz/beatoz-go/types"
@@ -12,20 +13,35 @@ import (
 // If the voter has already voted, it will be canceled.
 // This function is called from BeatozApp::BeginBlock.
 func (ctrler *GovCtrler) doPunish(targetAddr types.Address) (int64, xerrors.XError) {
-	slashedPower := int64(0)
+	var punishedProp []*proposal.GovProposal
+	defer func() {
+		for _, prop := range punishedProp {
+			_ = ctrler.proposalState.Set(v1.LedgerKeyProposal(prop.TxHash), prop, true)
+			fmt.Println("punishment", "txhash", prop.TxHash, "total.power", prop.TotalVotingPower, "majore.power", prop.MajorityPower)
+			for _, voter := range prop.Voters {
+				fmt.Println("voter", voter.Addr, "power", voter.Power)
+			}
 
+			d, _ := ctrler.proposalState.Get(v1.LedgerKeyProposal(prop.TxHash), true)
+			prop2, _ := d.(*proposal.GovProposal)
+			fmt.Println("read", "txhash", prop2.TxHash, "total.power", prop2.TotalVotingPower, "majore.power", prop2.MajorityPower)
+			for _, voter := range prop2.Voters {
+				fmt.Println("read voter", voter.Addr, "power", voter.Power)
+			}
+
+		}
+	}()
+
+	slashedPower := int64(0)
 	_ = ctrler.proposalState.Seek(v1.KeyPrefixProposal, true, func(key v1.LedgerKey, item v1.ILedgerItem) xerrors.XError {
 		prop, _ := item.(*proposal.GovProposal)
 		for _, v := range prop.Voters {
 			if bytes.Compare(v.Addr, targetAddr) == 0 {
-				// the voting power of `targetAddr` will be slashed and
+				// the voting power of `targetAddr` will be slashed, and
 				// the vote of `targetAddr` will be canceled.
 				slashed, _ := prop.DoPunish(targetAddr, ctrler.SlashRate())
 				slashedPower += slashed
-
-				if xerr := ctrler.proposalState.Set(v1.LedgerKeyProposal(prop.TxHash), prop, true); xerr != nil {
-					return xerr
-				}
+				punishedProp = append(punishedProp, prop)
 				break
 			}
 		}
