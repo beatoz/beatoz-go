201cb39ab9cbff28fb93bc7b1095402c42afba25
beatoz-kyle <kyle@beatoz.io>
2025-05-29 10:14:18 +0900

    test: Add benchmark codes for decimal.Decimal

--- libs/fxnum/fxnum_test.go
+++ libs/fxnum/fxnum_test.go
@@ -2,46 +2,82 @@ package fxnum
 
 import (
 	"github.com/robaho/fixed"
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/require"
 	"math/rand"
 	"testing"
 )
 
 var (
-	numCount  = 1000000
+	numCount  = 100
+	decNums   []decimal.Decimal
 	fixedNums []fixed.Fixed
+	fixedExps []fixed.Fixed
+	decExps   []decimal.Decimal
 	fxnumNums []FxNum
+	fxnumExps []FxNum
 )
 
 func init() {
 	for i := 0; i < numCount; i++ {
 		n := rand.Int63n(5000000000)
+		decNums = append(decNums, decimal.NewFromInt(n))
 		fixedNums = append(fixedNums, fixed.NewI(n, 0))
 		fxnumNums = append(fxnumNums, FromInt(n))
+
+		n = rand.Int63n(1000)
+		decExps = append(decExps, decimal.New(n, -3))
+		fixedExps = append(fixedExps, fixed.NewI(n, 3))
+		fxnumExps = append(fxnumExps, New(n, 3))
 	}
 }
 
-func Benchmark_Add(b *testing.B) {
+func Benchmark_FxNum_Add(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = fixedNums[i%numCount].Add(fixedNums[i%numCount])
+		_ = fxnumNums[i%numCount].Add(fxnumNums[(i+1)%numCount])
+	}
+}
+func Benchmark_Fixed_Add(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = fixedNums[i%numCount].Add(fixedNums[(i+1)%numCount])
+	}
+}
+func Benchmark_Decimal_Add(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = decNums[i%numCount].Add(decNums[(i+1)%numCount])
 	}
 }
 
-func Benchmark_Add_FxNum(b *testing.B) {
+func Benchmark_FxNum_Mul(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = fxnumNums[i%numCount].Add(fxnumNums[i%numCount])
+		_ = fxnumNums[i%numCount].Mul(fxnumNums[(i+1)%numCount])
+	}
+}
+func Benchmark_Fixed_Mul(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = fixedNums[i%numCount].Mul(fixedNums[(i+1)%numCount])
+	}
+}
+func Benchmark_Decimal_Mul(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = decNums[i%numCount].Mul(decNums[(i+1)%numCount])
 	}
 }
 
-func Benchmark_Mul(b *testing.B) {
+func Benchmark_FxNum_Pow(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = fixedNums[i%numCount].Mul(fixedNums[i%numCount])
+		_ = fxnumNums[i%numCount].Pow(fxnumExps[i%numCount])
+	}
+}
+func Benchmark_Fixed_Pow(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = FixedPow(fixedNums[i%numCount], fixedExps[i%numCount])
 	}
 }
 
-func Benchmark_Mul_FxNum(b *testing.B) {
+func Benchmark_Decimal_Pow(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = fxnumNums[i%numCount].Mul(fxnumNums[i%numCount])
+		_ = decNums[i%numCount].Pow(decExps[i%numCount])
 	}
 }
 
