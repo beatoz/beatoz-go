a703e272d1fc65e56d8f5a235a21d26468de7ed0
beatoz-kyle <kyle@beatoz.io>
2026-07-03 13:59:45 +0900

    feat!: Set BTIP27 and BTIP35 fork block height
    Set `testnetForkBlocks.BTIP27Block` to `194_850`
    Set `testnetForkBlocks.BTIP35Block` to `194_850`

--- types/params_test.go
+++ types/params_test.go
@@ -12,6 +12,6 @@ func Test_ForkBlocks(t *testing.T) {
 
 	require.False(t, IsBTIP27(chainId.Hex(), 0))
 	require.False(t, IsBTIP27(chainId.Hex(), 100))
-	require.True(t, IsBTIP27(chainId.Hex(), 100_000))
-	require.True(t, IsBTIP27(chainId.Hex(), 1_000_000))
+	require.True(t, IsBTIP27(chainId.Hex(), 194_850))
+	require.True(t, IsBTIP27(chainId.Hex(), 200_000))
 }
